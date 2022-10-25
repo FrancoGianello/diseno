3b76fb63f1385edaaf29eb6a1de2de25e598a7c3
Franco <francoacon98@gmail.com>
2022-10-25 21:40:40 +0200

    actualizasco

--- prototipo/js/index.js
+++ prototipo/js/index.js
@@ -1,9 +1,19 @@
+let http = new XMLHttpRequest();
 let padre = document.getElementById("pater");
-
-function crear(){
-    for (let i = 0; i < 250; i++) {
-        let hijo = document.createElement("div");
-        padre.appendChild(hijo);
+http.open('get', 'pokedexData.json', true);
+http.send();
+http.onload = function(){
+    if(this.readyState == 4 && this.status==200){
+        let datos = JSON.parse(this.responseText);
+        for (let value of datos) {
+            let hijo = document.createElement("div");
+            let claseTipo ="unico "+value.type;
+            if(value.type.length==2){
+                claseTipo="doble";
+            }
+            hijo.className=claseTipo + " "+ value.type[0] + " "+value.type[1];
+            hijo.innerHTML = '<p class="numero">'+value.id+'</p><p class="tipo">'+value.type+'</p><p class="nombre">'+value.name.english+'</p>';
+            padre.appendChild(hijo);
+        }
     }
 }
-crear();
