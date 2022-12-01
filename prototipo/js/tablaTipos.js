b4d55d8d08ec5926a85860ebd7766daf975dcffe
Franco <francoacon98@gmail.com>
2022-12-01 20:42:00 +0100

    hover hecho, falta refactorizarlo, dar otra vuelta quiza

--- prototipo/js/tablaTipos.js
+++ prototipo/js/tablaTipos.js
@@ -31,11 +31,12 @@ http.onload = function(){
                     if (datos.hasOwnProperty(keyY)) {
                         efectividad="efectividad"+datos[keyX][keyY];
                         if(datos[keyX][keyY]==0.5) efectividad="efectividadMedio"
-                        tr.innerHTML += '<td style="width: calc(100% / '+longitud+')" onhover="" class="'+efectividad+'" id="'+obtenerId(longitud)+'">x'+ datos[keyX][keyY]+'</td>';
+                        id =obtenerId(longitud);
+                        parametrosId = id.split(" ");
+                        tr.innerHTML += '<td style="width: calc(100% / '+longitud+')" onmouseover="destacarTipos('+parametrosId[0]+','+parametrosId[1]+')" onmouseout="resetearDestacado('+parametrosId[0]+','+parametrosId[1]+')" class="'+efectividad+'" id="'+id+'">x'+ datos[keyX][keyY]+'</td>';
                     }
                 }
                 padre.appendChild(tr);
-                // tabla.appendChild(tr);
             }
         }
         
@@ -52,4 +53,45 @@ function obtenerId(longitud){
     let id=contY +" "+contX;
     contX++;
     return id;
+}
+
+function destacarTipos(ejeY, ejeX){
+    //recorrer en la posicion X, tantas como Y se han especificado
+    for (let i = 0; i < ejeY; i++) {
+        casillaX = document.getElementById(i+" "+ejeX);
+        casillaX.style.position = "relative";
+        casillaX.style.boxShadow = "0 0 3px 5px #7AEEE3";   
+    }
+    //recorrer en la posicion Y, tantas como X se han especificado
+    for (let i = 0; i < ejeX; i++) { 
+        
+    casillaY = document.getElementById(ejeY+" "+i);
+    casillaY.style.position = "relative";
+    casillaY.style.boxShadow = "0 0 3px 5px #7AEEE3"
+    ;       
+    }
+    //destacar la casilla seleccionada
+    casilla = document.getElementById(ejeY+" "+ejeX);
+    casilla.style.position = "relative";
+    casilla.style.boxShadow = "0 0 3px 5px #7AEEE3"
+    ;
+}
+function resetearDestacado(ejeY, ejeX){
+    //recorrer en la posicion X, tantas como Y se han especificado
+    for (let i = 0; i < ejeY; i++) {
+        casillaX = document.getElementById(i+" "+ejeX);
+        casillaX.style.position = "static";
+        casillaX.style.boxShadow = "none";   
+    }
+    //recorrer en la posicion Y, tantas como X se han especificado
+    for (let i = 0; i < ejeX; i++) { 
+        
+    casillaY = document.getElementById(ejeY+" "+i);
+    casillaY.style.position = "static";
+    casillaY.style.boxShadow = "none";       
+    }
+    //destacar la casilla seleccionada
+    casilla = document.getElementById(ejeY+" "+ejeX);
+    casilla.style.position = "static";
+    casilla.style.boxShadow = "none";
 }
